e38243cb1ac2a05fef9b67060c7902635dfd60c5
Preston Harrison <preston@prestonharrison.com>
2022-11-03 19:50:41 +1000

    add requires

--- contracts/contracts/BettingPool/FeeDistribution.sol
+++ contracts/contracts/BettingPool/FeeDistribution.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.16;
 
 import "./Transferrer.sol";
 import "./Roles.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
 enum FeeType {
     Deposit,
@@ -11,18 +12,25 @@ enum FeeType {
 }
 
 abstract contract FeeDistribution is Transferrer, Roles {
+    using Address for address;
+
     address public feeReceipient;
+    address private immutable _factory;
 
     event ChangeFeeRecipient(address feeReceipient);
 
     constructor(address feeReceipient_) {
+        require(feeReceipient_ != address(0), "Fee recipient cannot be zero");
+        require(msg.sender.isContract(), "Msg.sender must be factory");
         feeReceipient = feeReceipient_;
+        _factory = msg.sender;
     }
 
     function setFeeRecipient(address feeReceipient_)
         external
         onlyRole(ADMIN_ROLE)
     {
+        require(feeReceipient_ != address(0), "Fee recipient cannot be zero");
         feeReceipient = feeReceipient_;
         emit ChangeFeeRecipient(feeReceipient_);
     }
--- contracts/contracts/BettingPool/Transferrer.sol
+++ contracts/contracts/BettingPool/Transferrer.sol
@@ -3,9 +3,12 @@ pragma solidity 0.8.16;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
 contract Transferrer {
     using SafeERC20 for IERC20;
+    using Address for address;
+
     /// An internal tracker of the token balance, to be used by
     /// the _transferInPoolToken method exclusively
     uint256 private _prevBalance;
@@ -17,6 +20,7 @@ contract Transferrer {
     address internal immutable self;
 
     constructor(address poolToken_) {
+        require(poolToken_.isContract(), "Pool token not contract");
         poolToken = poolToken_;
         self = address(this);
     }
