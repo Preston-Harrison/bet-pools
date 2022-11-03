e730a0a33f05ec2610a61f55f7d7deaff7941e79
Preston Harrison <preston@prestonharrison.com>
2022-11-03 19:54:30 +1000

    docs

--- contracts/contracts/BettingPool/FeeDistribution.sol
+++ contracts/contracts/BettingPool/FeeDistribution.sol
@@ -14,32 +14,36 @@ enum FeeType {
 abstract contract FeeDistribution is Transferrer, Roles {
     using Address for address;
 
-    address public feeReceipient;
+    /// The recipient of admin fees
+    address public adminFeeReceipient;
+    /// The address of the deployer of this contract
     address private immutable _factory;
 
     event ChangeFeeRecipient(address feeReceipient);
 
-    constructor(address feeReceipient_) {
-        require(feeReceipient_ != address(0), "Fee recipient cannot be zero");
+    constructor(address adminFeeReceipient_) {
+        require(adminFeeReceipient_ != address(0), "Admin fee recipient cannot be zero");
         require(msg.sender.isContract(), "Msg.sender must be factory");
-        feeReceipient = feeReceipient_;
+        adminFeeReceipient = adminFeeReceipient_;
         _factory = msg.sender;
     }
 
-    function setFeeRecipient(address feeReceipient_)
+    /// Sets the admin fee recipient
+    function setFeeRecipient(address adminFeeReceipient_)
         external
         onlyRole(ADMIN_ROLE)
     {
-        require(feeReceipient_ != address(0), "Fee recipient cannot be zero");
-        feeReceipient = feeReceipient_;
-        emit ChangeFeeRecipient(feeReceipient_);
+        require(adminFeeReceipient_ != address(0), "Fee recipient cannot be zero");
+        adminFeeReceipient = adminFeeReceipient_;
+        emit ChangeFeeRecipient(adminFeeReceipient_);
     }
 
+    /// Collects fees and returns the amount after fees have been taken
     function collectFees(uint256 amount, FeeType feeType)
         internal
         returns (uint256)
     {
-        // TODO fee calculations TBD. Something like this, probably
+        // TODO fee calculations TBD
         return amount;
     }
 }
