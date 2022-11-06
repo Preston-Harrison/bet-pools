de99d1cda0bc3f8d1e1f09693d53d86f24df6af4
Preston Harrison <preston@prestonharrison.com>
2022-11-06 10:47:01 +1000

    test liquidity

--- contracts/test/unit/LiquidityPool.test.ts
+++ contracts/test/unit/LiquidityPool.test.ts
@@ -1,7 +1,8 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
-import { ethers } from "hardhat";
-import { ERC20, ERC20__factory, TestToken, TestToken__factory } from "../../typechain-types";
+import { BigNumber } from "ethers";
+import hre, { ethers } from "hardhat";
+import { LiquidityToken, LiquidityToken__factory, TestToken, TestToken__factory } from "../../typechain-types";
 import { $LiquidityPool } from "../../typechain-types/contracts-exposed/BettingPool/LiquidityPool.sol/$LiquidityPool";
 import { $LiquidityPool__factory } from "../../typechain-types/factories/contracts-exposed/BettingPool/LiquidityPool.sol/$LiquidityPool__factory";
 import { bn } from "../utils";
@@ -9,8 +10,17 @@ import { bn } from "../utils";
 describe("LiquidityPool.sol", () => {
     let LiquidityPool: $LiquidityPool;
     let PoolToken: TestToken;
-    let LiquidityToken: ERC20;
+    let LiquidityToken: LiquidityToken;
     let deployer: SignerWithAddress;
+
+    const mintLiquidityTokens = async (address: string, amount: BigNumber) => {
+        await deployer.sendTransaction({
+            to: LiquidityPool.address,
+            value: bn(1)
+        });
+        const liquidityPool = await ethers.getSigner(LiquidityPool.address);
+        return LiquidityToken.connect(liquidityPool).mint(address, amount);
+    }
 
     beforeEach(async () => {
         [deployer] = await ethers.getSigners();
@@ -20,10 +30,15 @@ describe("LiquidityPool.sol", () => {
             deployer.address, 
             deployer.address
         );
-        LiquidityToken = ERC20__factory.connect(
+        LiquidityToken = LiquidityToken__factory.connect(
             await LiquidityPool.liquidityToken(),
             deployer
         );
+
+        await hre.network.provider.request({
+            method: "hardhat_impersonateAccount",
+            params: [LiquidityPool.address],
+        });
     });
 
     it("should increase and decrease reserved amounts", async () => {
@@ -51,7 +66,40 @@ describe("LiquidityPool.sol", () => {
         expect(await LiquidityPool.$getFreeBalance()).to.eq(bn(0));
     });
 
-    it("should transfer the correct amount for deposits", async () => {
-        const DEPOSIT_WITHDRAW_FEE = await LiquidityPool.DEPOSIT_WITHDRAW_FEE();
+    it("should calculate the correct amount for deposits & withdraws", async () => {
+        // 0 pool tokens, 0 liquidity tokens
+        let lAmount = await LiquidityPool.$calculateLiquidityTokenForDeposit(bn(10));
+        expect(lAmount).to.eq(bn(10));
+        lAmount = await LiquidityPool.$calculateLiquidityTokenForDeposit(bn(1000));
+        expect(lAmount).to.eq(bn(1000));
+
+        // 10 pool tokens, 10 liquidity tokens
+        await PoolToken.mint(LiquidityPool.address, bn(10));
+        await mintLiquidityTokens(deployer.address, bn(10));
+        lAmount = await LiquidityPool.$calculateLiquidityTokenForDeposit(bn(1000));
+        let pAmount = await LiquidityPool.$calculatePoolTokenForWithdraw(bn(10));
+        expect(pAmount).to.eq(bn(10))
+        expect(lAmount).to.eq(bn(1000));
+
+        // 10 pool tokens, 20 liquidity tokens
+        await mintLiquidityTokens(deployer.address, bn(10));
+        lAmount = await LiquidityPool.$calculateLiquidityTokenForDeposit(bn(10));
+        pAmount = await LiquidityPool.$calculatePoolTokenForWithdraw(bn(10));
+        expect(lAmount).to.eq(bn(20));
+        expect(pAmount).to.eq(bn(5))
+
+        // 40 pool tokens, 20 liquidity tokens
+        await PoolToken.mint(LiquidityPool.address, bn(30));
+        lAmount = await LiquidityPool.$calculateLiquidityTokenForDeposit(bn(10));
+        pAmount = await LiquidityPool.$calculatePoolTokenForWithdraw(bn(10));
+        expect(lAmount).to.eq(bn(5));
+        expect(pAmount).to.eq(bn(20));
+
+        // 40 pool tokens, 20 of which are reserved, 20 liquidity tokens
+        await LiquidityPool.$increaseReservedAmount(bn(20));
+        lAmount = await LiquidityPool.$calculateLiquidityTokenForDeposit(bn(10));
+        pAmount = await LiquidityPool.$calculatePoolTokenForWithdraw(bn(10));
+        expect(lAmount).to.eq(bn(10));
+        expect(pAmount).to.eq(bn(10));
     });
 });
